5d8146ed01513be3343ef7c0e7e2f8bd72f45319
KALP SHAH 043 <kalpshah916@gmail.com>
2025-08-29 23:24:14 +0530

    Create CALCULATE AREA AND PARIMETER OF SQUARE.C

---
+++ CALCULATE AREA AND PARIMETER OF SQUARE.C
@@ -0,0 +1,19 @@
+ #include<stdio.h>
+int main (){
+             int L,A,P2;
+    printf("Enter side: ");
+    scanf("%d",&L);
+    A=L*L;
+    P2=4*L;
+    printf("Area=%d Perimeter=%d\n",A,P2);
+
+ 
+
+
+
+
+
+    
+    return 0 ;
+    
+}  
