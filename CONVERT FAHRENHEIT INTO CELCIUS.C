a71df0426e594497f70777083d37f6bcf9836c60
KALP SHAH 043 <kalpshah916@gmail.com>
2025-08-29 23:22:44 +0530

    Create CONVERT FAHRENHEIT INTO CELCIUS.C

---
+++ CONVERT FAHRENHEIT INTO CELCIUS.C
@@ -0,0 +1,16 @@
+ #include<stdio.h>
+int main (){
+      float f2,c2;
+    printf("Enter fahrenheit: ");
+    scanf("%f",&f2);
+    c2=(5.0/9.0)*(f2-32);
+    printf("Celcius=%.2f\n",c2);
+
+
+
+
+    
+    return 0 ;
+    
+}  
+    
