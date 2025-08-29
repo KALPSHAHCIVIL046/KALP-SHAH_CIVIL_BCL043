99082bf29812be77eaea24a145a8350bb0e90fa7
KALP SHAH 043 <kalpshah916@gmail.com>
2025-08-29 23:09:54 +0530

    Create DIVIDE.C

---
+++ DIVIDE.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main (){
+float d1,d2,div;
+    printf("Enter two numbers: ");
+    scanf("%f%f",&d1,&d2);
+    div=d1/d2;
+    printf("Div=%f\n",div);
+    return 0 ;
+    
+}
