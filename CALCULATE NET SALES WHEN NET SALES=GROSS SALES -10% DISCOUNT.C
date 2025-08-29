1b45c26b72467a0f5ce79cabfe6ad65819ebda20
KALP SHAH 043 <kalpshah916@gmail.com>
2025-08-29 23:29:49 +0530

    Create CALCULATE NET SALES WHEN NET SALES=GROSS SALES -10% DISCOUNT.C

---
+++ CALCULATE NET SALES WHEN NET SALES=GROSS SALES -10% DISCOUNT.C
@@ -0,0 +1,16 @@
+{
+ float gss,nss;
+    printf("Enter gross sales: ");
+    scanf("%f",&gss);
+    nss=gss-(gss*0.10);
+    printf("Net sales=%.2f\n",nss);
+
+
+
+
+
+
+    
+    return 0 ;
+    
+}
