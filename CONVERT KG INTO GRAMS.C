4d0e77633e09ffb7be19586174b88998b6d0dc10
KALP SHAH 043 <kalpshah916@gmail.com>
2025-08-29 23:19:27 +0530

    Create CONVERT KG INTO GRAMS.C

---
+++ CONVERT KG INTO GRAMS.C
@@ -0,0 +1,14 @@
+  #include<stdio.h>
+int main (){
+  int kgg,gm;
+    printf("Enter kg: ");
+    scanf("%d",&kgg);
+    gm=kgg*1000;
+    printf("Grams=%d\n",gm);
+
+
+
+    
+    return 0 ;
+    
+}  
