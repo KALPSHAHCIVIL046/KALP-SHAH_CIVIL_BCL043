0849cf51f0e4ad9572493dd19b1052d42c4214cd
KALP SHAH 043 <kalpshah916@gmail.com>
2025-08-29 23:18:09 +0530

    Create CONVERT DOLLARS INTO POUNDS.C

---
+++ CONVERT DOLLARS INTO POUNDS.C
@@ -0,0 +1,16 @@
+ #include<stdio.h>
+int main (){
+   int dol2,pound;
+    printf("Enter dollars: ");
+    scanf("%d",&dol2);
+    pound=(dol2*48)/70;
+    printf("Pounds=%d\n",pound);
+
+
+
+
+
+    
+    return 0 ;
+    
+}  
