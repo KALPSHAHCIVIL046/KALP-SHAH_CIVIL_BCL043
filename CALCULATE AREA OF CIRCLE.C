41c289944515e9fdb770aa9447f66dfe7a20f00d
KALP SHAH 043 <kalpshah916@gmail.com>
2025-08-29 23:25:36 +0530

    Create CALCULATE AREA OF CIRCLE.C

---
+++ CALCULATE AREA OF CIRCLE.C
@@ -0,0 +1,16 @@
+  #include<stdio.h>
+int main (){
+ int r;
+    float area2;
+    printf("Enter radius: ");
+    scanf("%d",&r);
+    area2=3.14*r*r;
+    printf("Area=%.2f\n",area2);
+
+
+
+    
+    return 0 ;
+    
+}  
+    
