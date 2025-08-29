2b5b1d7e65ccef5285c7742c9859f2d4e11c75bb
KALP SHAH 043 <kalpshah916@gmail.com>
2025-08-29 23:28:22 +0530

    Create CALCULATE NET SALARY.C

---
+++ CALCULATE NET SALARY.C
@@ -0,0 +1,18 @@
+  #include<stdio.h>
+int main (){
+ float gs,ns;
+    printf("Enter gross salary: ");
+    scanf("%f",&gs);
+    ns=gs+(gs*0.10)-(gs*0.03);
+    printf("Net salary=%.2f\n",ns);
+
+
+
+
+
+
+    
+    return 0 ;
+    
+}  
+    
