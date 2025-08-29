c8472f8a235abfc5a52d262463b127cf5ff9c7e3
KALP SHAH 043 <kalpshah916@gmail.com>
2025-08-29 23:23:21 +0530

    Create CALCULATE INTEREST.C

---
+++ CALCULATE INTEREST.C
@@ -0,0 +1,17 @@
+  #include<stdio.h>
+int main (){
+      int P,R,N,I;
+    printf("Enter P R N: ");
+    scanf("%d %d %d",&P,&R,&N);
+    I=(P*R*N)/100;
+    printf("Interest=%d\n",I);
+ 
+
+
+
+
+
+    
+    return 0 ;
+    
+}  
