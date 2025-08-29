54eb95c1185db59d2ed87a39d5234b0de0b75165
KALP SHAH 043 <kalpshah916@gmail.com>
2025-08-29 23:27:43 +0530

    Create CALCULATE AREA OF TRIANGLE.C

---
+++ CALCULATE AREA OF TRIANGLE.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main (){
+ int h2,l2;
+    float area3;
+    printf("Enter height and base: ");
+    scanf("%d %d",&h2,&l2);
+    area3=(h2*l2)/2.0;
+    printf("Area=%.2f\n",area3);
+
+
+
+
+    
+    return 0 ;
+    
+}  
