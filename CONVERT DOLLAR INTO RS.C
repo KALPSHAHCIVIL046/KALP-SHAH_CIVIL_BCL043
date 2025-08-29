f5d851a0449e356ccba203b8c4cc49cfc9c890f5
KALP SHAH 043 <kalpshah916@gmail.com>
2025-08-29 23:13:52 +0530

    Create CONVERT DOLLAR INTO RS.C

---
+++ CONVERT DOLLAR INTO RS.C
@@ -0,0 +1,13 @@
+  #include<stdio.h>
+int main (){
+      int dollar,rs;
+    printf("Enter dollars: ");
+    scanf("%d",&dollar);
+    rs=dollar*85;
+    printf("Rupees=%d\n",rs);
+
+
+    
+    return 0 ;
+    
+}  
