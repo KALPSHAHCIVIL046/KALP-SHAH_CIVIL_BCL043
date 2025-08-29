98d5604a8c9c4fef2b3705bdd3bc5533383adbab
KALP SHAH 043 <kalpshah916@gmail.com>
2025-08-29 23:25:00 +0530

    Create CALCULATE AREA AND PERIMETER OF RECTANGLE.C

---
+++ CALCULATE AREA AND PERIMETER OF RECTANGLE.C
@@ -0,0 +1,14 @@
+  #include<stdio.h>
+int main (){
+int l,b,area,peri;
+    printf("Enter length and breadth: ");
+    scanf("%d %d",&l,&b);
+    area=l*b;
+    peri=2*(l+b);
+    printf("Area=%d Perimeter=%d\n",area,peri);
+
+    
+    return 0 ;
+    
+}  
+    
