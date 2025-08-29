debb5f94195ab9c5fabd4ba8030054007d7096d2
KALP SHAH 043 <kalpshah916@gmail.com>
2025-08-29 23:21:33 +0530

    Create CONVERT CELCIUS INTO FAHRENITE.C

---
+++ CONVERT CELCIUS INTO FAHRENITE.C
@@ -0,0 +1,15 @@
+ #include<stdio.h>
+int main (){
+      float c,f;
+    printf("Enter celcius: ");
+    scanf("%f",&c);
+    f=(9.0/5.0*c)+32;
+    printf("Fahrenheit=%.2f\n",f);
+
+
+
+    
+    return 0 ;
+    
+}  
+    
